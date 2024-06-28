5df024fff50b335b22f18614fef792be22c401fe
Sheerak <sheerakmegerdichian@gmail.com>
2024-06-28 11:22:36 -0700

    added the table to adduser

--- react-admin-dashboard-master/src/scenes/usermanager/index.jsx
+++ react-admin-dashboard-master/src/scenes/usermanager/index.jsx
@@ -13,10 +13,52 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { ColorModeContext, useMode } from "../../theme";
+import {useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+
+import { DataGrid } from '@mui/x-data-grid';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import scroll from '../../components/scroll';
 
 
-// TODO remove, this demo shouldn't need to reset the theme.
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
 
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 
 export default function SignUp() {
@@ -28,13 +70,42 @@ export default function SignUp() {
       password: data.get('password'),
     });
   };
-  const [theme, colorMode] = useMode();
-
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
   return (
+    <>
+    <scroll>
+    <TableContainer component={Paper} sx={{ maxHeight: 540 }}>
+      <Table sx={{ minWidth: 650 }}  aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell> Username </TableCell>
+            <TableCell align="right">Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } ,
+              backgroundColor: colors.primary[400],}}
+
+            >
+              <TableCell component="th" scope="row">{row.name}</TableCell>
+              <TableCell align="right"><button>Delete User</button ></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+</scroll>
+
+
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
+
         <Box
           sx={{
             marginTop: 8,
@@ -85,5 +156,6 @@ export default function SignUp() {
         </Box>
       </Container>
     </ThemeProvider>
+    </>
   );
 }
